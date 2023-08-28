a8e33a49f2c30564ecc5cb5aefd795f185a554a6
Kate Zaharova <15071981ktz@gmail.com>
2023-08-28 21:54:32 +0300

    Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,23 @@
-export const App = () => {
-  return (
-    <div
+import { Layout } from "./Layout";
+import { GlobalStyle } from "./GlobalStyle";
+import { Component } from "react";
+import { Form } from "./Form";
+import { ContactList } from "./ContactList";
+
+
+export class App extends Component {
+state = {
+    contacts: [],
+    filter: "",
+  };
+
+
+  render() {
+
+
+
+return (
+    <Layout
       style={{
         height: '100vh',
         display: 'flex',
@@ -8,9 +25,15 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      }}>
+    <h1>React homework template</h1>
+    <Form />
+    <ContactList/>
+    <GlobalStyle/>
+    </Layout>
   );
+  };
+
+  
+    
 };
